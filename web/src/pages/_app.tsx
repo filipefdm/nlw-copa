75445dfe68923a4876360bd8cd36954f6c0e31d4
filipefdm <femotta96@gmail.com>
2022-11-04 12:09:19 -0300

    :tada: day 4 finished

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import "../styles/global.css";
-import type { AppProps } from "next/app";
+import '../styles/global.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
+
+export default MyApp
